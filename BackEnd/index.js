749178b68f542004451bb4b6200a2bbf7060e0b2
JRafaelLobo <jose.lobo@unitec.edu>
2024-05-26 16:02:27 -0600

    ahora el backend si se actualiza

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Buenas</h1>')
+})
+
+app.listen(port, () => {
+    console.log('Port is ' + port)
+})
