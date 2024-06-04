0ec1bd8f669bf2bbad999f0645aba3928b68cde8
Marcela Tovar <marcelatovar.flores@gmail.com>
2024-06-04 13:56:36 -0600

    Conexion a la API

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,34 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const mongoURI = 'mongodb+srv://admin:6Ikry9U4a88k1ktY@mininetflixdatabase.kgwvbmp.mongodb.net/?retryWrites=true&w=majority&appName=MiniNetflixDatabase';
-const port = 5001;
+const port = 3000; 
 
-//Conexion a cors
-app.use(cors())
+// Configuración de CORS
+app.use(cors());
 
-//Rafa Base
-//User admin
-//password: 6Ikry9U4a88k1ktY
-//Enlace de Conexion: mongodb+srv://admin:6Ikry9U4a88k1ktY@mininetflixdatabase.kgwvbmp.mongodb.net/?retryWrites=true&w=majority&appName=MiniNetflixDatabase
+const apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDA0NGUwNWY5ZWUwYjRhODk5ZmJlYmI2NGVlZGRmNCIsInN1YiI6IjY2NWY2OTRiNzUyZWQ1YjBlYTdkNWI2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bZL-TdUZdqjUPNDAFdssr8RzL2DIGvJpp0N40sesXGU";
 
-//Marcela BAse
-//User: tovarmarcela518
-//Password: KaHwYrVXNZct328D
-//Enlace de Conexion: mongodb+srv://tovarmarcela518:<KaHwYrVXNZct328D>@mininetflix.kbk7hsd.mongodb.net/?retryWrites=true&w=majority&appName=MiniNetflix
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('No se pudo conectar a MongoDB', err));
 
-  app.options('*', cors())
-
 app.get('/', (req, res) => {
-    res.send('<h1>Buenas</h1>')
-})
+  res.send('<h1>Buenas</h1>');
+});
+
+axios.get('https://api.themoviedb.org/3/movie/157336?api_key=14044e05f9ee0b4a899fbebb64eeddf4')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
 app.listen(port, () => {
-    console.log('Port is ' + port)
-})
+  console.log('Server running on port ' + port);
+});
